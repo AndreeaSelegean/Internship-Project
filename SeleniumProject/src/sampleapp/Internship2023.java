13263fc7dceeaff2e4dad70c6be171c7a7e7924e
AndreeaS <140639074+AndreeaSelegean@users.noreply.github.com>
2023-07-26 19:41:50 +0300

    Add files via upload

---
+++ SeleniumProject/src/sampleapp/Internship2023.java
@@ -0,0 +1,30 @@
+package sampleapp;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import javax.print.DocFlavor;
+
+public class Internship2023 {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+
+
+       // System.setProperty("webdriver.chrome.driver","C:\\Users\\andreeaselegean\\Downloads\\chromedriver.exe");
+       //WebDriver driver=new ChromeDriver();
+       //driver.get("http://contract-management.evozon.com/login");
+
+    }
+
+
+    public int addition(){
+        int result = 2 + 3;
+        return result;
+
+    }
+
+}
+
+
+
---
+++ SeleniumProject/src/sampleapp/JavaBasics.java
@@ -0,0 +1,37 @@
+package sampleapp;
+
+public class JavaBasics {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Andreea");
+
+        int num1 = 10;
+        int num2 = 5;
+        int sum = num1 + num2;
+        System.out.println("Suma celor doua numere este: " + sum);
+
+
+        double num3 = 10;
+        double num4 = 3;
+        double result = num3 / num4;
+        System.out.println("Rezultatul impartirii este" + result);
+
+        // a. -5 +8 * 6;
+        int a = -5 + 8 * 6;
+        System.out.println("a. -5 + 8 * 6 = " + a);
+
+        int b = ( 55 + 9 ) % 9;
+        System.out.println("b. (55 + 9) % 9 = " + b);
+
+        double c = 20 + -3 * 5 / 8.0;
+        System.out.println("c. 20 + -3 * 5 / 8 = " + c);
+
+        int d = 5 + 15 / 3 * 2 - 8 % 3 ;
+        System.out.println("d. 5 + 15 / 3 * 2 - 8 % 3 = " + d);
+
+
+    }
+
+
+
+}
